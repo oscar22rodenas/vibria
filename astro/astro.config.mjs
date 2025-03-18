df5b1665e35cd42cb12719e6b232103cecfc0ddd
rrodenas <rrodenas@alumnat.copernic.cat>
2025-03-18 20:34:54 +0100

    implementacion de multiidioma

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'; // Importa el plugin correcto
 
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind()],
 });
