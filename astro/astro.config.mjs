bc127891058f9c9d11d038ad94796e247f52d567
Adrián Bernabeu Malia <abernabeu>
2025-03-19 17:04:37 +0100

    Modificamos el config de astro para que funcione el build

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'; // Importa el plugin correcto
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
+  adapter: node({mode: 'standalone'}),
   integrations: [tailwind()],
 });
