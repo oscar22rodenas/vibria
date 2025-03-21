95fd243c5e472223a32a78eca1e4370557ee27dc
rrodenas <rrodenas@alumnat.copernic.cat>
2025-03-21 16:36:23 +0100

    Afegir estils generals a global.css implementacio de la part de canal de difusio en astro amb els seus respectius estils

--- astro/src/lib/wp.js
+++ astro/src/lib/wp.js
@@ -11,3 +11,20 @@ export const getPageInfo = async (slug) => {
 
   return data.acf;
 };
+
+export const getImageUrl = async (imageId) => {
+  if (!imageId) return "";
+
+  try {
+    const response = await fetch(`${apiURL}/media/${imageId}`);
+    if (!response.ok) {
+      throw new Error(`No se encontró la imagen con ID ${imageId}`);
+    }
+    const data = await response.json();
+    return { source_url: data.source_url, alt_text: data.alt_text };
+    
+  } catch (error) {
+    console.error("Error obteniendo imagen:", error);
+    return "";
+  }
+};
--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -1,12 +1,39 @@
-// tailwind.config.cjs
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{astro,html,js,ts,jsx,tsx}", // Asegúrate de que Tailwind escanee todos los archivos relevantes
-    ],
+    content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: "#058279",
+          secondary: "#67D19F",
+          yellow: "#FFC721",
+          orange: "#F57E26",
+          blue: "#81AEF6",
+          red: "#FC8975",
+          purple: "#9747FF",
+          white: "#FFFFFF",
+          black: "#000000",
+        },
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+        fontSize: {
+          h1: ["30px", { lineHeight: "30px" }],
+          h2: ["25px", { lineHeight: "18px" }], // Cambia a 30px si ocupa dos líneas
+          h3: ["20px", { lineHeight: "18px" }], // Cambia a 24px si ocupa dos líneas
+          h4: ["17px", { lineHeight: "30px" }], // Algunos tienen 18px
+          p: ["15px", { lineHeight: "18px" }],
+          button: ["20px", { lineHeight: "30px", textTransform: "uppercase" }],
+        },
+        fontWeight: {
+          400: "400",
+          500: "500",
+          600: "600",
+          700: "700",
+          800: "800",
+        },
+      },
     },
     plugins: [],
-  }
+  };
   
