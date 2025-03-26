36eae8cdb955db4945e21565733f9325b6cc54e5
rrodenas <rrodenas@alumnat.copernic.cat>
2025-03-26 20:27:26 +0100

    Modificacio de variables de les imatges en tots els components i afegir xarxes socials

--- astro/src/lib/wp/index.js
+++ astro/src/lib/wp/index.js
@@ -1,6 +1,7 @@
 import { getPageInfo } from "./getPageInfo.js";
 import { getImageInfo } from "./getImageInfo.js";
 import { getSlidesInfo } from "./getSlidesInfo.js";
+import { getRRSSInfo } from "./getRRSSInfo.js";
 
-export { getPageInfo, getImageInfo, getSlidesInfo };
+export { getPageInfo, getImageInfo, getSlidesInfo, getRRSSInfo };
 
