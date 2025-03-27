6287bee56890efa115c3177c16dcff1d578dcaf0
Adrián Bernabeu Malia <abernabeu>
2025-03-27 17:35:53 +0100

    Arreglado error cuando se habre el menu que no salga su icono

--- astro/src/lib/wp/index.js
+++ astro/src/lib/wp/index.js
@@ -1,10 +1,10 @@
 import { getPageInfo } from "./getPageInfo.js";
 import { getImageInfo } from "./getImageInfo.js";
-import { getSlideInfo } from "./getSlideInfo.js";
+import { getSlidesInfo } from "./getSlidesInfo.js";
 import { getRRSSInfo } from "./getRRSSInfo.js";
 
 import { getCategoriesInfo } from "./getCategoriesInfo.js";
 
 
-export { getPageInfo, getImageInfo, getSlideInfo, getRRSSInfo, getCategoriesInfo  };
+export { getPageInfo, getImageInfo, getSlidesInfo, getRRSSInfo, getCategoriesInfo  };
 
