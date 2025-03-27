5c9656193fe50427f19b909cecee82e247f1886f
Adrián Bernabeu Malia <abernabeu>
2025-03-27 16:55:15 +0100

    Menu corregido con los datos reales del wordpress i multiidioma

--- astro/src/lib/wp/index.js
+++ astro/src/lib/wp/index.js
@@ -1,6 +1,7 @@
 import { getPageInfo } from "./getPageInfo.js";
 import { getImageInfo } from "./getImageInfo.js";
 import { getSlideInfo } from "./getSlideInfo.js";
+import { getCategoriesInfo } from "./getCategoriesInfo.js";
 
-export { getPageInfo, getImageInfo, getSlideInfo };
+export { getPageInfo, getImageInfo, getSlideInfo, getCategoriesInfo };
 
