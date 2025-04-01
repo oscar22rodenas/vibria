79911557a5763a3769f1c596f369879575d476ec
rrodenas <rrodenas@alumnat.copernic.cat>
2025-04-01 20:46:35 +0200

    modificacio de arxius de carpetas

--- astro/src/lib/wp/index.js
+++ astro/src/lib/wp/index.js
@@ -2,9 +2,8 @@ import { getPageInfo } from "./getPageInfo.js";
 import { getImageInfo } from "./getImageInfo.js";
 import { getSlidesInfo } from "./getSlidesInfo.js";
 import { getRRSSInfo } from "./getRRSSInfo.js";
-import { queFem } from "./que-fem.json";
 import { getCategoriesInfo } from "./getCategoriesInfo.js";
 
 
-export { getPageInfo, getImageInfo, getSlidesInfo, getRRSSInfo, getCategoriesInfo, queFem };
+export { getPageInfo, getImageInfo, getSlidesInfo, getRRSSInfo, getCategoriesInfo };
 
