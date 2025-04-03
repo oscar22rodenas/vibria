55212219adeecf194b8ddfb5c6f746f2926c7e78
rrodenas <rrodenas@alumnat.copernic.cat>
2025-04-03 16:25:10 +0200

    Modificacio de rutas en el slides y alguns cambis respecte al getStaticPaths

--- astro/src/lib/wp/getStaticPaths.js
+++ astro/src/lib/wp/getStaticPaths.js
@@ -1,3 +1,5 @@
+import { extractLang } from "./extractLang";
+
 export async function getStaticPaths() {
     const languages = ["ca", "es", "en"]; // Idiomas soportados
 
@@ -5,17 +7,6 @@ export async function getStaticPaths() {
     const pages = await pagesRes.json();
     
     let paths = [];
-
-    // Función para extraer idioma del slug (Ej: "home-es" → { lang: "es", baseSlug: "home" })
-    function extractLang(slug) {
-        const match = slug.match(/-(ca|es|en)$/); // Buscar sufijo de idioma
-        if (match) {
-            const lang = match[1];
-            const baseSlug = slug.replace(/-(ca|es|en)$/, ""); // Quitar sufijo de idioma
-            return { lang, baseSlug };
-        }
-        return null;
-    }
     
     pages.forEach(page => {
         const extracted = extractLang(page.slug);
@@ -28,5 +19,4 @@ export async function getStaticPaths() {
     });
     
     return paths;
-
 }
