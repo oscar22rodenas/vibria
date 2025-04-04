f358dfc096a40afd09403a54c1d97c4989c96c66
rrodenas <rrodenas@alumnat.copernic.cat>
2025-04-04 17:56:13 +0200

    Modificació de noms de arxius

--- astro/src/lib/wp/getStaticPaths.js
+++ astro/src/lib/wp/getStaticPaths.js
@@ -3,11 +3,11 @@ import { extractLang } from "./extractLang";
 export async function getStaticPaths() {
     const languages = ["ca", "es", "en"]; // Idiomas soportados
 
-    const pagesRes = await fetch("http://localhost:8000/wp-json/wp/v2/pages?_fields=acf,slug&per_page=100");
+    const pagesRes = await fetch("http://localhost:8000/wp-json/wp/v2/pages?slug=home-ca&_fields=acf,slug&per_page=100");
     const pages = await pagesRes.json();
     
     let paths = [];
-    
+
     pages.forEach(page => {
         const extracted = extractLang(page.slug);
         if (extracted && languages.includes(extracted.lang)) {
