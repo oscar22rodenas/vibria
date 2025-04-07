3eb8e2b77aee733b94680e15bc7a98a556b786c5
rrodenas <rrodenas@alumnat.copernic.cat>
2025-04-07 19:35:32 +0200

    Afegir estils a ofertes i  modificacio dels arxius de slide

--- astro/src/lib/wp/index.js
+++ astro/src/lib/wp/index.js
@@ -3,7 +3,8 @@ import { getSlidesInfo } from "./getSlidesInfo.js";
 import { getRRSSInfo } from "./getRRSSInfo.js";
 import { getCategoriesInfo } from "./getCategoriesInfo.js";
 import { getPostsInfo } from "./getPostsInfo.js";
-import { getPageById } from "./getPagebyId.js";
+import { getPageById } from "./getPageById.js";
+import { getOfertesInfo } from "./getOfertesInfo.js";
 
-export { getImageInfo, getSlidesInfo, getRRSSInfo, getCategoriesInfo, getPostsInfo, getPageById };
+export { getImageInfo, getSlidesInfo, getRRSSInfo, getCategoriesInfo, getPostsInfo, getPageById, getOfertesInfo };
 
