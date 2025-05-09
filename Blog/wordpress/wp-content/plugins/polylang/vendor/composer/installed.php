f77985adfa88a0cb964ab67049d8812de2e2dfbd
Brian <arrua.dominguez.brian.tobias@alumnat.copernic.cat>
2025-05-09 17:30:38 +0200

    afegim la nova imatge de voluntariat italia

--- Blog/wordpress/wp-content/plugins/polylang/vendor/composer/installed.php
+++ Blog/wordpress/wp-content/plugins/polylang/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wpsyntex/polylang',
         'pretty_version' => '3.7.x-dev',
         'version' => '3.7.9999999.9999999-dev',
-        'reference' => 'c2060c7eab4d7a158d8a03d1e4009ec0725fbef1',
+        'reference' => 'c9cc69a8d359f38742a26cd353428f7d9d4f3d92',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'wpsyntex/polylang' => array(
             'pretty_version' => '3.7.x-dev',
             'version' => '3.7.9999999.9999999-dev',
-            'reference' => 'c2060c7eab4d7a158d8a03d1e4009ec0725fbef1',
+            'reference' => 'c9cc69a8d359f38742a26cd353428f7d9d4f3d92',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
