1c12d1f473bd92b37409792cf818e666724e3bdf
Adrián Bernabeu Malia <abernabeu>
2025-05-27 19:38:10 +0200

    Añadimos a cada pagina una plantilla por si se añade content

---
+++ astro/src/lib/wp/getPlaygroundInfo.js
@@ -0,0 +1,17 @@
+import { apiURL } from "./config.js";
+
+export const getPlaygroundInfo = async (lang, slug) => {
+  try {    
+    const responsePage = await fetch(`${apiURL}/pages?slug=${slug}&_fields=content`);
+    if (!responsePage.ok) {
+      throw new Error("Error al obtener la página");
+    }
+    const [pageData] = await responsePage.json();
+        return {
+          content: pageData.content.rendered || "",
+        };
+  } catch (error) {
+    console.error("Error obteniendo experiencies:", error);
+    return [];
+  }
+};
